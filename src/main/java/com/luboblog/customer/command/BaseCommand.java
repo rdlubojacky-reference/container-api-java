76f4da8bd4fcd2ed5434ad20efc17894ffa92d3e
Russell Lubojacky <russell.lubojacky@gmail.com>
2020-05-03 16:31:17 -0500

    [MRA-9] adding command classes

---
+++ src/main/java/com/luboblog/customer/command/BaseCommand.java
@@ -0,0 +1,13 @@
+package com.luboblog.customer.command;
+
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+public class BaseCommand<T> {
+
+    @TargetAggregateIdentifier
+    public final T id;
+
+    public BaseCommand(T id) {
+        this.id = id;
+    }
+}
